6531219663d0fc71330b331b61dae288b1084feb
Peter Pan <peter@never.lan>
2023-06-09 19:43:29 -0400

    reorganize, add init.sh

---
+++ init.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#/+++++++++++++++++ SHIKIGAMI SETUP v0.0.1 +++++\
+#| This script will install all dependencies    |
+#| required to run Shikigami. It will also      |
+#| create the config directory and copy the     |
+#| config files if they do not exist.           |
+#\+++++++++++++++++++++++++++++++++++++++++++++/
+#===========================<[ RVM INSTALL...
+if ! [ -d $HOME/.rvm ]; then
+        echo "Installing RVM..."
+        curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+        curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+        curl -sSL https://get.rvm.io | bash -s stable
+        bash -c "source $HOME/.rvm/scripts/rvm && rvm get head && rvm install 3.1"
+fi
+
+#===========================<[ NVM INSTALL...
+if ! [ -d $HOME/.nvm ]; then
+        echo "Installing NVM..."
+        mkdir $HOME/.nvm
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+        bash -c "source $HOME/.nvm/nvm.sh && nvm install 16"
+fi
+
+#===========================<[ CONFIG DIR CREATION...
+if ! [ -d $HOME/.config ]; then
+        echo "Creating config directory..."
+        mkdir $HOME/.config
+fi
+if ! [ -d $HOME/.config/shikigami ]; then
+        echo "Creating shikigami config directory..."
+        mkdir $HOME/.config/shikigami
+fi
+
+#===========================<[ CHECK FOR USER CONFIG...
+if ! [ -f ./config.yml ] && ! [ -f $HOME/.config/shikigami/config.yml ]; then
+        echo 'ERROR: config.yml does not exist, aborting!'
+        exit 1;
+fi #~WARNING~: the next line will ALWAYS overwrite with config.yml from the repo!
+if [ -f ./config.yml ]; then
+        echo "Copying shikigami config file..."
+        cp ./config.yml $HOME/.config/shikigami/config
+fi
+
+#===========================<[ INSTALL DEPENDENCIES...
+bash -c "source $HOME/.nvm/nvm.sh && npm install pm2 -g"
+gem install bundler
+bundle install
--- src/core.rb
+++ src/core.rb
@@ -1,9 +1,9 @@
 require 'socket'
 require './library/lib_config.rb'
 
-cfg = AppSettings.new
+$cfg = AppSettings.new
 
-server = TCPServer.new(cfg.get("shiki_host"), cfg.get("shiki_port")) # Server bind to port 1025
+server = TCPServer.new($cfg.get("shiki_host"), $cfg.get("shiki_port")) # Server bind to port 1025
 loop do
   client = server.accept    # Wait for a client to connect
   client.puts "Hello !"
