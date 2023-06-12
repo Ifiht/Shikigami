7072735f7c1b0d76f2c7e6a499bf4f51f8e7f3e0
Peter Pan <peter@never.lan>
2023-06-12 19:00:15 -0400

    remove core.rb, will contain sensitive info. moving queue to beanstalkd

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source 'https://rubygems.org'
 gem 'telegram-bot-ruby', '~> 1.0'
 gem 'listen', '~> 2.0'
 gem 'net-ping', '~> 1.7', '>= 1.7.8'
+gem 'httpx'
+gem 'pp'
 #gem 'rice' -- this is for torch-rb
 #gem 'torch-rb' -- to be added later
--- src/library/lib_habitica.rb
+++ src/library/lib_habitica.rb
@@ -1,3 +1,4 @@
+require 'pp'
 require 'uri'
 require 'json'
 require 'httpx'
@@ -11,13 +12,51 @@ class HabActions
   def joinQuest
     http = HTTPX.post("https://habitica.com/api/v3/groups/party/quests/accept",
       headers: {"x-api-user" => @habId, "x-api-key" => @habToken})
-    puts http.body if http.status == 200
+    puts PP.pp(JSON.parse(http.body)) if http.status == 200
   end
 
   def cron
     http = HTTPX.post("https://habitica.com/api/v3/cron", headers: {"x-api-user" => @habId, "x-api-key" => @habToken})
     if http.status == 200
-      puts JSON.parse(http.body)
+      puts PP.pp(JSON.parse(http.body))
+    else
+      puts "Error: #{http.status}"
+    end
+  end
+
+  def msgParty(msg)
+    http = HTTPX.post("https://habitica.com/api/v3/groups/party/chat",
+      headers: {"x-api-user" => @habId, "x-api-key" => @habToken},
+      form: {"message" => msg})
+    if http.status == 200
+      puts PP.pp(JSON.parse(http.body))
+    else
+      puts "Error: #{http.status}"
+    end
+  end
+
+  def getTasks
+    http = HTTPX.get("https://habitica.com/api/v3/tasks/user", headers: {"x-api-user" => @habId, "x-api-key" => @habToken})
+    if http.status == 200
+      puts PP.pp(JSON.parse(http.body))
+    else
+      puts "Error: #{http.status}"
+    end
+  end
+
+  def upCheckTask(taskId)
+    http = HTTPX.get("https://habitica.com/api/v3/tasks/#{taskId}/score/up", headers: {"x-api-user" => @habId, "x-api-key" => @habToken})
+    if http.status == 200
+      puts PP.pp(JSON.parse(http.body))
+    else
+      puts "Error: #{http.status}"
+    end
+  end
+
+  def downCheckTask(taskId)
+    http = HTTPX.get("https://habitica.com/api/v3/tasks/#{taskId}/score/down", headers: {"x-api-user" => @habId, "x-api-key" => @habToken})
+    if http.status == 200
+      puts PP.pp(JSON.parse(http.body))
     else
       puts "Error: #{http.status}"
     end
