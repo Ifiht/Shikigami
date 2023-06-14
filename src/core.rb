028e936966ccb70978d98533d6897d5d755b94ad
Peter Pan <peter@never.lan>
2023-06-14 17:04:08 -0400

    break out beanstalk into separate library

--- src/core.rb
+++ src/core.rb
@@ -1,11 +1,14 @@
 #=============<[ Gems ]>=============#
 require 'json'
-require 'beaneater'
+require 'socket'
 #==========<[ Local Libs ]>==========#
 require './library/lib_core_config.rb'
+require './library/lib_core_bstalk.rb'
 require './library/lib_habitica.rb'
 require './library/lib_telegram.rb'
 
+
+#[[[[[[ INITIALIZE CONFIG & ALL LIBRARY CLASSES HERE]]]]]]
 core_config = AppSettings.new
 beanstalk_host = core_config.get("beanstalk_host")
 beanstalk_port = core_config.get("beanstalk_port")
@@ -15,21 +18,47 @@ habitica_token = core_config.get("api_habitica_token")
 habitica = HabActions.new(habitica_usrid, habitica_token)
 #habitica.msgParty('Hello world.')
 
-thread_habitica = Thread.start do
-  bstalk_habitica = Beaneater.new("#{beanstalk_host}\:#{beanstalk_port}")
-  bstalk_habitica.tubes.watch!('tb_manual')
-  loop do
-    job = bstalk_habitica.tubes.reserve
-    if job.exists?
-      begin
-        eval job.body
-      rescue SyntaxError
-        puts "SyntaxError: #{job.body}"
-      ensure
-        job.delete
-      end
-    end
-  end
-end
 
-thread_habitica.join
+#[[[[[[ DEFINE CHECK PORT OPEN ]]]]]]
+def port_open?(ip, port)
+  Timeout::timeout(2) do
+    begin
+      TCPSocket.new(ip, port).close
+      true
+    rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH, SocketError
+      false
+    rescue Timeout::Error
+      false
+    end#begin
+  end#do
+end#def
+
+#[[[[[[ PORT CHECK FOR SHIKIGAMI EXTERNAL RESOURCES ]]]]]]
+if port_open?(beanstalk_host, beanstalk_port)
+
+  #[[[[[[ DEFINE THREADS ]]]]]]
+  thread_manual = Thread.start do
+    bstalk_manual = BeanLoop.new(beanstalk_host, beanstalk_port, "tb_manual")
+    bstalk_manual.run
+  end#thread_manual
+
+  thread_telegram = Thread.start do
+    bstalk_telegram = BeanLoop.new(beanstalk_host, beanstalk_port, "tb_telegram")
+    bstalk_telegram.run
+  end#thread_telegram
+
+  thread_filesystem = Thread.start do
+    bstalk_filesystem = BeanLoop.new(beanstalk_host, beanstalk_port, "tb_filesystem")
+    bstalk_filesystem.run
+  end#thread_filesystem
+
+  #[[[[[[ JOIN THREADS ]]]]]]
+  thread_manual.join
+  thread_telegram.join
+  thread_filesystem.join
+
+else
+  puts "Cannot initialize threads, beanstalkd not reachable."
+  exit 2
+end#if
+
---
+++ src/library/lib_core_bstalk.rb
@@ -0,0 +1,29 @@
+#=============<[ Gems ]>=============#
+require 'beaneater'
+#==========<[ Local Libs ]>==========#
+require './library/lib_core_config.rb'
+
+class BeanLoop
+  def initialize(beanstalk_host="127.0.0.1", beanstalk_port=9999, beanstalk_tube="default")
+    @beanstalk_host = beanstalk_host
+    @beanstalk_port = beanstalk_port
+    @beanstalk_tube = beanstalk_tube
+  end#def
+
+  def run
+    @bstalk = Beaneater.new("#{@beanstalk_host}\:#{@beanstalk_port}")
+    @bstalk.tubes.watch!(@beanstalk_tube)
+    loop do
+      job = @bstalk.tubes.reserve
+      if job.exists?
+        begin
+          eval job.body
+        rescue SyntaxError
+          puts "SyntaxError: #{job.body}"
+        ensure
+          job.delete
+        end#begin
+      end#if
+    end#loop
+  end#def
+end#class
