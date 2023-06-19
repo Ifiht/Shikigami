716f2bf8757fc7d7b6ab58f6ea33303a7ef21151
Peter Pan <peter@never.lan>
2023-06-19 07:43:19 -0400

    fix env in cron wrapper

--- src/events/evt_wrapper.sh
+++ src/events/evt_wrapper.sh
@@ -2,6 +2,6 @@
 ##&% Change to current script directory
 cd "${0%/*}"
 ##&% Source ruby environment script from rvm
-source $HOME/.rvm/wrappers/default
+source $HOME/.rvm/environments/ruby-3.1.4@shikigami
 ##&% Execute the event of choice (manual here):
-ruby evt_manual.rb "log_to_pm2 'Hello, Dave.'"
+$HOME/.rvm/wrappers/ruby-3.1.4@shikigami/ruby evt_manual.rb "log_to_pm2 'Hello, Dave.'"
