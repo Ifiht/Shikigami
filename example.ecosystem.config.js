35846ab180343e1222e91834a61b70d0848fcc88
Peter Pan <peter@never.lan>
2023-06-22 20:51:31 -0400

    add back example files

---
+++ example.ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps : [{
+    name   : "beanstalk",
+    script: './beanstalkd/beanstalkd',
+    args: '-l 127.0.0.2 -p 9999'
+  },
+  {
+    name   : "core.rb",
+    script: 'ruby',
+    args: './src/core.rb',
+    watch: true,
+    restart_delay: 500
+  }],
+
+  deploy : {
+    remote : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/main',
+      repo : 'https://github.com/Ifiht/Shikigami.git',
+      path : '$HOME/Shikigami',
+      'pre-deploy-local': './init.sh',
+      'post-deploy' : 'npm install pm2 -g && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
