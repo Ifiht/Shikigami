31dea8f820018afefdd8333e42dfc04353c1a62e
Peter Pan <peter@never.lan>
2023-06-29 18:36:12 -0400

    fix core.rb path

--- src/core.rb
+++ src/core.rb
@@ -6,7 +6,7 @@ require "io/console"
 require "concurrent"
 require "require_all"
 #==========<[ Local Libs ]>==========#
-require_rel "library/*.rb"
+require_rel "lib/*.rb"
 
 #@@@@@@ USERS MAY EDIT THEIR SETTINGS BELOW:
 #[[[[[[ INITIALIZE CONFIG & ALL LIBRARY CLASSES HERE]]]]]]
