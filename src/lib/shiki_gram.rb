35017ed3ad550ab77e6d36b5f838ff635bd7cc80
Peter Pan <peter@never.lan>
2024-01-17 20:09:34 -0500

    add shiki gram

---
+++ src/lib/shiki_gram.rb
@@ -0,0 +1,15 @@
+require "yaml"
+
+class ShikiGram
+  def wrap_msg(msg)
+    msg_str = msg.to_yaml
+    msg64 = Base64.encode64(msg_str)
+    return msg64
+  end
+
+  def open_msg(msg64)
+    msg_str = Base64.decode64(msg64)
+    msg = YAML.load(msg_str)
+    return msg
+  end
+end
