928b6ae93c0013914ed7c0f83c1b64af414c9eff
Peter Pan <peter@never.lan>
2024-02-11 21:23:03 -0500

    tentative working habitica module

--- src/modules/habitica/habitica.rb
+++ src/modules/habitica/habitica.rb
@@ -6,36 +6,32 @@ require_rel "../../lib/app_settings"
 require_relative "lib_habitica"
 
 sgram = ShikiGram.new
+@habit = HabActions.new
 core_config = AppSettings.new
 beanstalk_host = core_config.get("beanstalk_host")
 beanstalk_port = core_config.get("beanstalk_port")
 
-#[[[[[[ PORT CHECK FOR SHIKIGAMI EXTERNAL RESOURCES ]]]]]]
-if port_open?(beanstalk_host, beanstalk_port)
 
-  #[[[[[[ DEFINE THREADS ]]]]]]
-  core_threads = []
-  job_threads = []
-  a = [] # empty holder array for jobs
-  semaphore = Mutex.new
-  bstalk = Beaneater.new("#{beanstalk_host}\:#{beanstalk_port}")
-  bstalk.tubes.find("core") # also creates the tube
-  bstalk.tubes.watch!("core")
+bstalk = Beaneater.new("#{beanstalk_host}\:#{beanstalk_port}")
+bstalk.tubes.find("habitica") # also creates the tube
+bstalk.tubes.watch!("habitica")
 
-core_threads << Thread.new {
-  loop do
-    job = bstalk.tubes.reserve
-    if job.exists?
-      str = sgram.open_msg(job.body)
-      log_to_pm2("Received job: #{str}")
-      semaphore.synchronize { # Only modify job_threads in semaphore
-        job_threads << Thread.new {
-          eval_string(str)
-          Thread.current.exit
-        }
-      }
-      job.delete
-    end #if
-    sleep 0.00024
-  end #loop
-}
+def log_to_pm2(message)
+  $stdout.puts message
+  $stdout.flush
+end
+
+loop do
+  job = bstalk.tubes.reserve
+  if job.exists?
+    str = sgram.open_msg(job.body)
+    log_to_pm2("Received job: #{str}")
+    begin
+      eval_string(str)
+    rescue e
+      log_to_pm2("Rescued job: #{e}")
+    end
+    job.delete
+  end #if
+  sleep 0.00024
+end #loop
