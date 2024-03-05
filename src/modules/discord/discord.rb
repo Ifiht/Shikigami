51cb844721913fb0ac3df6939bac464a8a906291
Peter Pan <peter@never.lan>
2024-03-04 22:47:38 -0500

    disord integration...

--- src/modules/discord/discord.rb
+++ src/modules/discord/discord.rb
@@ -8,10 +8,10 @@ require_rel "../../lib/app_settings"
 core_config = AppSettings.new
 beanstalk_host = core_config.get("beanstalk_host")
 beanstalk_port = core_config.get("beanstalk_port")
-habitica_token = core_config.get("habitica_token")
+discord_token = core_config.get("api_discord_token")
 
 # join url: https://discordapp.com/oauth2/authorize?&client_id=CLIENT_ID&scope=bot&permissions=274878155840
-bot = Discordrb::Bot.new token: habitica_token
+bot = Discordrb::Bot.new token: discord_token
 
 bot.message(with_text: "Ping!") do |event|
   event.respond "Pong!"
