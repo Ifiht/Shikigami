816e8a89e41b92dbef7fe532d18b23b1ca4cd29a
Peter Pan <peter@never.lan>
2024-04-20 19:03:45 -0400

    update discord app setting

--- src/modules/discord/discord.rb
+++ src/modules/discord/discord.rb
@@ -3,12 +3,11 @@ require "discordrb"
 require "require_all"
 #==========<[ Local Libs ]>==========#
 #require_rel "../../lib/shiki_gram"
-#require_rel "../../lib/app_settings"
+require_rel "../../lib/app_settings"
 require_rel "../../lib/shiki_stdlib"
 
 shiki = Shiki.new("discord")
-beanstalk_host = core_config.get("beanstalk_host")
-beanstalk_port = core_config.get("beanstalk_port")
+core_config = AppSettings.new
 discord_token = core_config.get("api_discord_token")
 
 # join url: https://discordapp.com/oauth2/authorize?&client_id=CLIENT_ID&scope=bot&permissions=274878155840
