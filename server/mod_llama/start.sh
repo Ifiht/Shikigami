060fc6d70fa49ca8df6fdbeaab6e797adfebb649
Peter Pan <peter@never.lan>
2026-03-30 09:20:32 -0400

    fix shebang??

--- server/mod_llama/start.sh
+++ server/mod_llama/start.sh
@@ -1,4 +1,4 @@
-$!/bin/bash
+#!/bin/bash
 
 # Use this for interactive testing:
 # llama-cli -m ./weights/Devstral-Small-2-24B-Instruct-2512/Devstral-Small-2-24B-Instruct-2512-Q8_0.gguf -ngl 99 --no-kv-offload -ctk q8_0 -ctv q8_0 -fa on -c 131072 -sys $1
